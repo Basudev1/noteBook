dcaf873c8c0afb45e18d4975c1fedd1d9e477ff7
Basudev <murabasudev@gmail.com>
2021-12-09 12:51:21 +0530

    learnt to use uselocation in react js

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,10 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import noteContext from '../context/notes/noteContext';
 
 function About() {
-    const a = useContext(noteContext);
-    useEffect(() => {
-        a.update();
-    }, []);
     return (
         <div>
-            About Page {a.state.name} and he is in {a.state.class}
+            About Page 
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,23 +1,8 @@
 import React, {useState} from 'react';
 import noteContext from './noteContext';
 const NoteState = (props) =>{
-    const s1 = {
-        "name": "Basu",
-        "class": "B8",
-    }
-
-const [state, setstate] = useState(s1)
-const update = () =>{
-    setTimeout(() => {
-        setstate({
-            "name": "Dev",
-            "class": "B1",
-        })
-    }, 1000);
-}
-
-    return (
-<noteContext.Provider value={{state,update}}>
+        return (
+<noteContext.Provider value={{}}>
     {props.children}
 </noteContext.Provider>
     )
