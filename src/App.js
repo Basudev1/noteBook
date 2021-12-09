0fe249b581f9b6c1fe8f44b52ddbb411490a97a3
Basudev <murabasudev@gmail.com>
2021-12-09 10:50:01 +0530

    Setup react router and created files for context api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import React from 'react'
-// import ReactDOM from 'react-dom'
+import { BrowserRouter as Router,Routes, Route,} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h2>
-          Hello React Js
-        </h2>
-      </header>
-    </div>
+    <>
+<Router>
+        <Navbar />
+        <Routes >
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+           <h2>MyNotebook - Next Generation Notes in the cloud</h2>
+        </div>
+    )
+}
