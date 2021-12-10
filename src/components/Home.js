d4628ef7e32c307302555528337e1a647e2cc1bf
Basudev <murabasudev@gmail.com>
2021-12-10 08:02:21 +0530

    fetching notes  using dummy data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
-
+import React, {useContext} from "react";
+import noteContext from "../context/notes/noteContext";
 export const Home = () => {
+    const context = useContext(noteContext)
+    const {notes, setnotes} = context;
   return (
     <div className="container my-3">
       <h1>Add a Note</h1>
@@ -45,7 +47,12 @@ export const Home = () => {
           </button>
         </form>
       </div>
+      <div className="container my-3">
       <h2>Your Notes</h2>
+      {notes.map((note)=>{
+          return note.title
+      })}
+      </div>
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,29 @@
 import React, {useState} from 'react';
 import noteContext from './noteContext';
 const NoteState = (props) =>{
+    const inititalNotes =  [
+            {
+              "_id": "61ac7901e89519e1670c01db",
+              "user": "61a885470a5f30ad46ddc453",
+              "title": "My title",
+              "description": "A Nice Description here",
+              "tag": "Personal",
+              "date": "1638693121446",
+              "__v": 0
+            },
+            {
+                "_id": "61ac7901e89519e1670c01db",
+                "user": "61a885470a5f30ad46ddc453",
+                "title": "My title",
+                "description": "A Nice Description here",
+                "tag": "Personal",
+                "date": "1638693121446",
+                "__v": 0
+              }
+          ]
+    const [notes, setnotes] = useState(inititalNotes);
         return (
-<noteContext.Provider value={{}}>
+<noteContext.Provider value={{notes, setnotes}}>
     {props.children}
 </noteContext.Provider>
     )
