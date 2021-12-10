3bfedd1ef120da7ee60e371297be4d64c616eb8b
Basudev <murabasudev@gmail.com>
2021-12-10 08:27:48 +0530

    make some changes to arrange better note items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,53 @@
-import React, {useContext} from "react";
-import noteContext from "../context/notes/noteContext";
+import React from 'react';
+import Notes from './Notes';
 export const Home = () => {
-    const context = useContext(noteContext)
-    const {notes, setnotes} = context;
   return (
-    <div className="container my-3">
-      <h1>Add a Note</h1>
+    <>
       <div className="container my-3">
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
+        <h1>Add a Note</h1>
+        <div className="container my-3">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" class="form-text">
+                We'll never share your email with anyone else.
+              </div>
             </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div class="mb-3 form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+        <Notes />
       </div>
-      <div className="container my-3">
-      <h2>Your Notes</h2>
-      {notes.map((note)=>{
-          return note.title
-      })}
-      </div>
-    </div>
+    </>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -14,7 +14,37 @@ const NoteState = (props) =>{
             {
                 "_id": "61ac7901e89519e1670c01db",
                 "user": "61a885470a5f30ad46ddc453",
-                "title": "My title",
+                "title": "My title 1",
+                "description": "A Nice Description here",
+                "tag": "Personal",
+                "date": "1638693121446",
+                "__v": 0
+              }
+              ,
+            {
+                "_id": "61ac7901e89519e1670c01db",
+                "user": "61a885470a5f30ad46ddc453",
+                "title": "My title 2",
+                "description": "A Nice Description here",
+                "tag": "Personal",
+                "date": "1638693121446",
+                "__v": 0
+              }
+              ,
+            {
+                "_id": "61ac7901e89519e1670c01db",
+                "user": "61a885470a5f30ad46ddc453",
+                "title": "My title 4",
+                "description": "A Nice Description here",
+                "tag": "Personal",
+                "date": "1638693121446",
+                "__v": 0
+              }
+              ,
+            {
+                "_id": "61ac7901e89519e1670c01db",
+                "user": "61a885470a5f30ad46ddc453",
+                "title": "My title 5",
                 "description": "A Nice Description here",
                 "tag": "Personal",
                 "date": "1638693121446",
