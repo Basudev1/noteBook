4513da19c664fab0c7df168eb4dabeb753276dfd
Basudev <murabasudev@gmail.com>
2022-02-05 13:06:16 +0530

    Added Some About Information

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
-import React from 'react';
-
+import React from "react";
 
 function About() {
-    return (
-        <div>
-            About Page 
-        </div>
-    )
+  return (
+    <div>
+      <b>About Page:</b>
+      <br />
+      <br />
+      <b>
+        This is a Simple Note Taking app Built with React Js, Node Js, MongoDB
+        <br />
+        In This App A user Can Register and Login to their Account and Add Note,
+        Edit existing Notes and Delete their Notes
+      </b>
+    </div>
+  );
 }
 
-export default About
+export default About;
